68c1d2137489cf5a222b4f73a2e79caa71fe3ff6
villaricadianne <91996994+villaricadianne@users.noreply.github.com>
2022-08-13 17:32:18 -0700

    Added delete button and feature

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
 "use strict";
 
-let myLibrary = [
-  // {
-  //   title: "The Hobbit",
-  //   author: "JJR Tolkien",
-  //   pages: 223,
-  //   read: false,
-  // },
-  // {
-  //   title: "Hooked",
-  //   author: "Nir Eyal",
-  //   pages: 567,
-  //   read: false,
-  // },
-  // {
-  //   title: "After Dark",
-  //   author: "Haruki Murakami",
-  //   pages: 1233,
-  //   read: false,
-  // },
-];
+let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  // this.title: document.getElementById("title").value;
-  // this.author: document.getElementById("author").value;
-  // this.pages: document.getElementById("pages").value;
-  // this.read: document.getElementById("read").value;
+function Book(id, title, author, pages, read) {
+  this.id = Date.now();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  // this.addInfo = function () {
-  //   return title, author, pages, read;
-  // };
 }
 
 const addBookToLibrary = (event) => {
@@ -41,43 +16,56 @@ const addBookToLibrary = (event) => {
   let myLibraryItem = new Book();
 
   // Create each object
-  (myLibraryItem.title = document.getElementById("title").value),
+  myLibraryItem.id,
+    (myLibraryItem.title = document.getElementById("title").value),
     (myLibraryItem.author = document.getElementById("author").value),
     (myLibraryItem.pages = document.getElementById("pages").value),
     (myLibraryItem.read = document.getElementById("read").value),
-    // let myLibraryItem = { // this work, but use a constructor
-    //   title: document.getElementById("title").value,
-    //   author: document.getElementById("author").value,
-    //   pages: document.getElementById("pages").value,
-    //   read: document.getElementById("read").value,
-    // };
-
     myLibrary.push(myLibraryItem);
   document.querySelector("form").reset();
 
-  // Display on screen make a table
-  // myLibrary.forEach((myLibraryItem) => {
+  console.log(myLibraryItem.read);
+
+  // Display in a table
   const tr = document.createElement("tr");
   const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("class", "delete");
   deleteButton.innerHTML = "Delete";
+
+  //delete an item
+  deleteButton.addEventListener("click", function () {
+    const itemTitle = this.nextSibling.textContent;
+    const itemPos = myLibrary.findIndex((item) => item.title == itemTitle);
+
+    myLibrary.splice(itemPos, 1);
+
+    this.parentNode.parentNode.removeChild(tr);
+  });
 
   // TODO: replace checkbox for true or false
   // const checkbox = document.createElement("input");
 
-  Object.values(myLibraryItem).forEach((value) => {
-    let td = document.createElement("td");
-    td.innerHTML = value;
-    tr.prepend(deleteButton);
-    tr.appendChild(td);
-    // tr.appendChild(checkbox);
-  });
-  document.querySelector("tbody").appendChild(tr);
-  // });
-};
-document.getElementById("form-btn").addEventListener("click", addBookToLibrary);
+  Object.values(myLibraryItem)
+    .slice(1)
+    .forEach((value) => {
+      let td = document.createElement("td");
+      td.innerHTML = value;
+      tr.prepend(deleteButton);
+      tr.appendChild(td);
+      // tr.appendChild(checkbox);
+    });
 
-// work on adding to the list
+  document.querySelector("tbody").appendChild(tr);
+
+  // console.log(myLibrary);
+  console.log(myLibraryItem);
+};
+
+document.getElementById("form-btn").addEventListener("click", addBookToLibrary);
 
 // delete a list
 
 // toggle boolean value of read within the object
+
+// value true is being stored?
+// find a way to delete array element within delete button
